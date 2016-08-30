d117477c2bd3259369a0fbe2f1ff060998fc1652
Arne Brasseur <arne@arnebrasseur.net>
2016-08-30 16:51:34 +0200

    Implement POST /, which creates a new contact

--- src/rolodex/core.clj
+++ src/rolodex/core.clj
@@ -1,16 +1,32 @@
 (ns rolodex.core
-  (:require [ring.adapter.jetty :refer [run-jetty]]
+  (:require [clojure.edn :as edn]
+            [ring.adapter.jetty :refer [run-jetty]]
             [rolodex.generator :as gen]))
 
 (defonce contacts (atom (into {} (map (juxt :id identity)
                                       (map gen/contact gen/names)))))
 
+(defn- get-all-contacts []
+  {:status 200
+   :headers {"Content-Type" "application/edn; charset=UTF-8"}
+   :body (prn-str (vals @contacts))})
+
+(defn- create-new-contact [req]
+  (if-let [instream (:body req)]
+    (let [body (slurp instream)
+          contact (edn/read-string body)]
+      (swap! contacts assoc (:id contact) contact)
+
+      {:status 201
+       :headers {"Content-Type" "application/edn; charset=UTF-8"}
+       :body (prn-str contact)})))
+
 (defn handler "request -> response"
   [req]
-  (if (and (= (:request-method req) :get) (= (:uri req) "/"))
-    {:status 200
-     :headers {"Content-Type" "application/edn; charset=UTF-8"}
-     :body (prn-str (vals @contacts))}))
+  (if (= (:uri req) "/")
+    (case  (:request-method req)
+      :get (get-all-contacts)
+      :post (create-new-contact req))))
 
 (defonce server
   (run-jetty #'handler {:port 3000
