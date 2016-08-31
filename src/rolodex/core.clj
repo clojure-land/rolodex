6a5046a386edd07c2c99e9e0104d2bbebfe08040
Arne Brasseur <arne@arnebrasseur.net>
2016-08-31 14:05:32 +0200

    Pull out EDN middleware, and implement GET /:id

--- src/rolodex/core.clj
+++ src/rolodex/core.clj
@@ -1,7 +1,8 @@
 (ns rolodex.core
   (:require [clojure.edn :as edn]
             [ring.adapter.jetty :refer [run-jetty]]
-            [rolodex.generator :as gen]))
+            [rolodex.generator :as gen])
+  (:import java.util.UUID))
 
 (defonce contacts (atom (into {} (map (juxt :id identity)
                                       (map gen/contact gen/names)))))
@@ -11,23 +12,45 @@
    :body (vals @contacts)})
 
 (defn- create-new-contact [req]
+  (let [contact (:params req)]
+    (swap! contacts assoc (:id contact) contact)
+    {:status 201
+     :body contact}))
+
+(defn- parse-params [req]
   (if-let [instream (:body req)]
-    (let [body (slurp instream)
-          contact (edn/read-string body)]
-      (swap! contacts assoc (:id contact) contact)
-      {:status 201
-       :body contact})))
+    (assoc req :params (-> instream slurp edn/read-string))
+    req))
+
+(defn wrap-edn "handler -> handler"
+  [handler]
+  (fn [req]
+    (let [req (parse-params req)
+          res (handler req)]
+      (if res
+        (-> res
+            (assoc-in [:headers "Content-Type"] "application/edn; charset=UTF-8")
+            (update :body prn-str))))))
+
+(def uuid-uri-pattern #"\A/(\p{XDigit}{8}-\p{XDigit}{4}-\p{XDigit}{4}-\p{XDigit}{4}-\p{XDigit}{12})\z")
+
+(defn- get-contact [uuid]
+  {:status 200
+   :body (@contacts uuid)})
 
 (defn handler "request -> response"
   [req]
-  (-> (if (= (:uri req) "/")
-        (case  (:request-method req)
-          :get (get-all-contacts)
-          :post (create-new-contact req)))
-      (assoc-in [:headers "Content-Type"] "application/edn; charset=UTF-8")
-      (update :body prn-str)))
-
+  (let [uri (:uri req)
+        method (:request-method req)]
+    (if (= uri "/")
+      (case method
+        :get (get-all-contacts)
+        :post (create-new-contact req))
+      (if-let [uuid (second (re-find uuid-uri-pattern uri))]
+        (let [uuid (UUID/fromString uuid)]
+          (case method
+            :get (get-contact uuid)))))))
 
 (defonce server
-  (run-jetty #'handler {:port 3000
-                        :join? false}))
+  (run-jetty (wrap-edn #'handler) {:port 3000
+                                   :join? false}))
